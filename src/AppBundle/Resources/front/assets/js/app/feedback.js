3233fc7670886a8c60e5f498d8311c4dc66b6541
Fu1grim <torchagb@ya.ru>
2016-05-25 17:37:42 +0300

    Grand Finale Fixes 4

--- src/AppBundle/Resources/front/assets/js/app/feedback.js
+++ src/AppBundle/Resources/front/assets/js/app/feedback.js
@@ -3,7 +3,9 @@
 import "jquery";
 import "jquery-validation";
 import "jquery-mask-plugin";
+import Menu from "../components/menu";
 import Loader from "../components/loader";
 
+new Menu();
 new Loader();
 $("#feedback-form").validate();
